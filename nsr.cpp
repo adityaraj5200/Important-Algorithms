2a691d4b263370d809f27f2eb7f0f8832cc07337
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2022-01-08 19:55:07 +0530

    Nearest Smaller to Right

---
+++ nsr.cpp
@@ -0,0 +1,12 @@
+vector<int> nsr(vector<int> &vec){
+    int n = vec.size();
+    vector<int> result(n,n);
+    stack<int> stk;
+    for(int i=n-1;i>=0;i--){
+        while(!stk.empty() && vec[i] <= vec[stk.top()])
+            stk.pop();
+        result[i] = !stk.empty() ? stk.top() : n;
+        stk.push(i);
+    }
+    return result;
+}
