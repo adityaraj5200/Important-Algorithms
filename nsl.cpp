12ddf404d65afcb5a890a46b2ab5056cc3de73d2
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2022-01-08 19:56:23 +0530

    Nearest Smaller to Left

---
+++ nsl.cpp
@@ -0,0 +1,12 @@
+vector<int> nsl(vector<int> &vec){
+    int n = vec.size();
+    vector<int> result(n,-1);
+    stack<int> stk;
+    for(int i=0;i<n;i++){
+        while(!stk.empty() && vec[i] <= vec[stk.top()])
+            stk.pop();
+        result[i] = !stk.empty() ? stk.top() : -1;
+        stk.push(i);
+    }
+    return result;
+}
