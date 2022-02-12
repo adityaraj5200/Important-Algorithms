5649713a06a43257f5f0f63a9cf7ac72fdc4b919
Aditya Raj <72252036+adityaraj5200@users.noreply.github.com>
2022-02-12 22:51:13 +0530

    Gave Indentation so that after copying and pasting I don't lost indentation.

--- Nearest Smaller to Right.cpp
+++ Nearest Smaller to Right.cpp
@@ -1,12 +1,12 @@
-vector<int> nsr(vector<int> &vec){
-    const int n = vec.size();
-    vector<int> result(n,n);
-    stack<int> stk;
-    for(int i=n-1;i>=0;i--){
-        while(!stk.empty() && vec[i] <= vec[stk.top()])
-            stk.pop();
-        result[i] = !stk.empty() ? stk.top() : n;
-        stk.push(i);
+    vector<int> nsr(vector<int> &vec){
+        const int n = vec.size();
+        vector<int> result(n,n);
+        stack<int> stk;
+        for(int i=n-1;i>=0;i--){
+            while(!stk.empty() && vec[i] <= vec[stk.top()])
+                stk.pop();
+            result[i] = !stk.empty() ? stk.top() : n;
+            stk.push(i);
+        }
+        return result;
     }
-    return result;
-}
